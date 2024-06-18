a5c679a2e962377cbf22046a53c0e33f49be11bb
anastasijas2101 <anastasijas101@gmail.com>
2024-06-18 17:09:45 +0200

    Add TitlePage on Home

--- src/features/Home/components/TitlePage.jsx
+++ src/features/Home/components/TitlePage.jsx
@@ -5,14 +5,14 @@ const TitlePage = () => {
     return (
         <div className='title-page'>
             <div className='left-title-page'>
-                <h1>Explore The World Of Music</h1>
+                <h1>Explore the World Of Music</h1>
                 <hr />
                 <p>Come with us on a journey through pop, rock and
                     everything in between. Discover all that is hidden between the lines...</p>
             </div>
             <div className='right-title-page'>
                 <img src={logoImage} alt='logoImage' />
-                <h1>TuneHub</h1>
+                <h1>Tune<span>Hub</span></h1>
             </div>
         </div>
     )
