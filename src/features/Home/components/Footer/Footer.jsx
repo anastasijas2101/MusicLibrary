f5352ff0e43b217a0eb612d9dfc861a0ad502422
anastasijas2101 <anastasijas101@gmail.com>
2024-06-20 14:52:15 +0200

    Adding index file to the project

--- src/features/Home/components/Footer/Footer.jsx
+++ src/features/Home/components/Footer/Footer.jsx
@@ -1,31 +1,29 @@
 import './Footer.style.css'
-import youtube from '../../../../assets/youtube-icon.webp'
-import instagram from '../../../../assets/instagram.png'
-import xIcon from '../../../../assets/x-icon.png'
-import snapchat from '../../../../assets/snapchat.png'
-import facebook from '../../../../assets/facebook.webp'
+import { Youtube, Instagram, XIcon, Snapchat, Facebook } from '../../../../assets'
 
 const Footer = () => {
     return (
         <footer className='footer-style'>
             <h3>For more fun content, follow us on:</h3>
             <div className='footer-image-div'>
-                <a href='https://www.youtube.com/'><img src={youtube} alt='YouTube' /></a>
+                <a href='https://www.youtube.com/'>
+                    <Youtube />
+                </a>
                 <span>|</span>
                 <a href='https://www.instagram.com/'>
-                    <img src={instagram} alt='Instagram' />
+                    <Instagram />
                 </a>
                 <span>|</span>
                 <a href='https://x.com/?lang=en'>
-                    <img src={xIcon} alt='X' />
+                    <XIcon />
                 </a>
                 <span>|</span>
                 <a href='https://www.snapchat.com/'>
-                    <img src={snapchat} alt='Snapchat' />
+                    <Snapchat />
                 </a>
                 <span>|</span>
                 <a href='https://www.facebook.com/'>
-                    <img src={facebook} alt='Facebook' />
+                    <Facebook />
                 </a>
             </div>
         </footer>
--- src/features/Navigation/TitleNavigation.jsx
+++ src/features/Navigation/TitleNavigation.jsx
@@ -1,10 +1,10 @@
 import '../Navigation/styles/TitleNavigation.style.css'
-import logoIcon from '../../assets/logo-image.png'
+import { Logo } from '../../assets'
 
 const TitleNavigation = () => {
     return (
         <div className="title-navigation">
-            <img src={logoIcon} alt='logoIcon' />
+            <Logo />
             <h1>Tune<span>Hub</span></h1>
         </div>
     )
