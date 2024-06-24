7ec80ade1ac382eeca1f1af4d42cdafbff2aa7a0
anastasijas2101 <anastasijas101@gmail.com>
2024-06-24 12:34:52 +0200

    Add registration page

--- src/features/Home/components/ThirdSlide.jsx
+++ src/features/Home/components/ThirdSlide.jsx
@@ -2,8 +2,15 @@ import '../style/ThirdSlide.style.css'
 import Button from '../../../components/Button/Button'
 import { SoundWaves, Notes, Dancing } from '../../../assets'
 import Footer from './Footer/Footer'
+import { useNavigate } from 'react-router-dom'
 
 const ThirdSlide = () => {
+    const router = useNavigate();
+
+    const handleClick = () => {
+        router('/registration');
+    }
+
     return (
         <div className='full-third-slide'>
             <div className='third-slide'>
@@ -13,7 +20,7 @@ const ThirdSlide = () => {
                     <Dancing className='notes-dancing' />
                 </div>
                 <p>Join us today and never miss any news from music world. Also, let us help you keep all of your favorite songs in one place.</p>
-                <Button buttonText='Subscribe to TuneHub' className='button-join' />
+                <Button buttonText='Subscribe to TuneHub' className='subscribe-button' onClick={handleClick} />
             </div>
             <Footer />
         </div>
