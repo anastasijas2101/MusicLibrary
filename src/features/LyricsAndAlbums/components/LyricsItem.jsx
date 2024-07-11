57d360b8fec6d4bc0f0e7ec4dd981519166ac3d0
anastasijas2101 <anastasijas101@gmail.com>
2024-07-11 14:42:16 +0200

    Add heart style

--- src/features/LyricsAndAlbums/components/LyricsItem.jsx
+++ src/features/LyricsAndAlbums/components/LyricsItem.jsx
@@ -1,15 +1,27 @@
 import './LyricsItem.style.css'
 import Button from '../../../components/Button/Button'
-import { useNavigate } from 'react-router-dom'
 import heart from '../../../assets/favorites.svg'
+import { useNavigate } from 'react-router-dom'
 import { useFavoritesStore } from '../../../stores/favorites.store'
+import { useState } from 'react'
 
 const LyricsItem = ({ song }) => {
     const navigate = useNavigate();
-    const { addFavorites } = useFavoritesStore();
+    const { addFavorites, deleteFavorites } = useFavoritesStore();
+    const [filled, setFilled] = useState();
 
     const handleDetails = (id) => {
         navigate(`${id}`)
+    }
+
+    const handleFavorites = () => {
+        setFilled(!filled);
+
+        if (filled) {
+            deleteFavorites(song.id);
+        } else {
+            addFavorites(song);
+        }
     }
 
     return (
@@ -21,7 +33,7 @@ const LyricsItem = ({ song }) => {
                 <p>Release date: {song.date}</p>
                 <div className='buttons'>
                     <Button buttonText='See Lyrics' className='lyrics-button' onClick={() => handleDetails(song.id)} />
-                    <img src={heart} alt='favorites' onClick={() => addFavorites(song)} />
+                    <img src={heart} alt='favorites' onClick={() => handleFavorites()} className={filled ? 'heart-filled' : ''} />
                 </div>
             </div>
         </div>
